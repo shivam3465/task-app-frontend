784d1675ce572ffd869c9cdb8c61a67326394057
shivam kumar <meforcoding@gmail.com>
2025-01-24 06:11:28 +0530

    added key prop

--- src/app/common/component/Skeleton/Task.item.skeleton.component.jsx
+++ src/app/common/component/Skeleton/Task.item.skeleton.component.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const TaskItemSkeleton = () => {
 	return [1, 2, 3].map((c) => (
-		<div className="flex flex-col p-4 rounded shadow h-[80px] bg-[#f4f4f4] shimmer-container">
+		<div
+			key={c}
+			className="flex flex-col p-4 rounded shadow h-[80px] bg-[#f4f4f4] shimmer-container">
 			<div className="h-[24px] rounded-[4px]  w-[50%] my-2 shimmer"></div>
 			<div className="h-[24px] rounded-[4px] w-[70%] shimmer"></div>
 		</div>
